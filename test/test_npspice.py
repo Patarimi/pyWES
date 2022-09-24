96c9ec17f5dd8ca2cce835f8b0435a338d462543
Patarimi <mpqqch@gmail.com>
2022-09-24 19:01:19 +0200

    revert ngspice raw parser

--- test/test_npspice.py
+++ test/test_npspice.py
@@ -4,10 +4,10 @@ from wrapper.spice_wrapper import write_result
 
 
 def test_ngspice():
-    sim = NGSpice(sim_path=f"{getcwd()}/simulators/Spice64/bin/ngspice_con.exe")
+    sim = NGSpice(sim_path="C:/Users/Manouda/PycharmProjects/fastAPI/simulators/Spice64/bin/ngspice_con.exe")
 
-    out = sim.run(f"{getcwd()}/test/schem_test.net", f"{getcwd()}/test/")
+    sim.run(f"{getcwd()}/test/schem_test.net", f"{getcwd()}/test/")
 
-    res = sim.serialize_result(out)
+    res = sim.serialize_result(f"{getcwd()}/test/sim_batch.out")
 
     write_result(res, f"{getcwd()}/test/out.h5py")
--- wrapper/ngspice.py
+++ wrapper/ngspice.py
@@ -3,7 +3,6 @@ from pydantic import FilePath, DirectoryPath
 from typing import List
 from .spice_wrapper import SpiceWrapper, ResultDict
 import numpy as np
-import tempfile
 
 
 class NGSpice(SpiceWrapper):
@@ -13,44 +12,45 @@ class NGSpice(SpiceWrapper):
         )
 
     def run(self, _spice_file: FilePath, log_folder: DirectoryPath):
-        out = tempfile.NamedTemporaryFile(delete=False)
-        proc = run([self.path, "-b", "-r", out.name, _spice_file], stdout=PIPE, stderr=STDOUT)
-        with open(log_folder+"sim.log", "bw") as f:
+        with open(_spice_file, "r") as cir:
+            proc = run([self.path, "-s"], stdin=cir, stdout=PIPE, stderr=PIPE)
+        with open(log_folder+"sim_server.out", "bw") as f:
             f.write(proc.stdout)
-        return out
+        with open(log_folder+"sim_log.out", "bw") as f:
+            f.write(proc.stderr)
 
     def serialize_result(self, result_file: FilePath):
         num_var: int
         num_pts: int
         var_names: List[str] = list()
         results: ResultDict = dict()
-        f = result_file
-        for line in f:
-            if b"No. Variables:" in line:
-                num_var = int(line.split(b" ")[2])
-                print(f"{num_var} variables expected")
-                break
-        for line in f:
-            if b"No. Points:" in line:
-                num_pts = int(line.split(b" ")[2])
-                print(f"{num_pts} points expected")
-                break
-        for line in f:
-            if b"Variables" in line:
-                continue
-            var_names.append(line.split(b"\t")[2])
-            if len(var_names) >= num_var:
-                print(var_names)
-                break
-        for name in var_names:
-            results[name] = np.zeros(num_pts)
-        i = 0
-        for line in f:
-            if b"Values" in line:
-                continue
-            if i == 0:
-                results[var_names[i]] = float(line.split(b"\t")[2])
-            else:
-                results[var_names[i]] = float(line)
-            i = 0 if (i >= num_var - 1) else i + 1
+        with open(result_file, 'r') as f:
+            for line in f:
+                if 'No. Variables:' in line:
+                    num_var = int(line.split(' ')[2])
+                    print(f"{num_var} variables expected")
+                    break
+            for line in f:
+                if 'No. Points:' in line:
+                    num_pts = int(line.split(' ')[2])
+                    print(f"{num_pts} points expected")
+                    break
+            for line in f:
+                if 'Variables' in line:
+                    continue
+                var_names.append(line.split('\t')[2])
+                if len(var_names) >= num_var:
+                    print(var_names)
+                    break
+            for name in var_names:
+                results[name] = np.zeros(num_pts)
+            i = 0
+            for line in f:
+                if 'Values' in line:
+                    continue
+                if i == 0:
+                    results[var_names[i]] = float(line.split('\t')[2])
+                else:
+                    results[var_names[i]] = float(line)
+                i = 0 if (i >= num_var - 1) else i+1
         return results

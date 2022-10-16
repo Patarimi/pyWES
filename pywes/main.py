da45db2423d8831de60dd243e399e6e7317f3584
Patarimi <mpqqch@gmail.com>
2022-10-16 14:41:04 +0200

    fix import after re-organisation

--- pywes/main.py
+++ pywes/main.py
@@ -18,9 +18,9 @@ def uvicorn_handler(command, side: str):
     if command == "start":
         print(f"starting {side} side")
         if side == "server":
-            uvicorn.run("server:Server", host="127.0.0.1", port=5000, log_level="info")
+            uvicorn.run("pywes.server:Server", host="127.0.0.1", port=5000, log_level="info")
         else:
-            uvicorn.run("client:Client", host="127.0.0.1", port=5050, log_level="info")
+            uvicorn.run("pywes.client:Client", host="127.0.0.1", port=5050, log_level="info")
 
 
 if __name__ == "__main__":
--- test/test_npspice.py
+++ test/test_npspice.py
@@ -4,7 +4,7 @@ from asyncio import run
 
 
 def test_ngspice():
-    sim = NGSpice(sim_path="C:/Users/Manouda/PycharmProjects/fastAPI/simulators/Spice64/bin/ngspice_con.exe")
+    sim = NGSpice(sim_path=f"{getcwd()}/simulators/Spice64/bin/ngspice_con.exe")
     run(sim.run(f"{getcwd()}/test/schem_test.net", f"{getcwd()}/test/"))
 
     assert len(sim.results.keys()) == 6
